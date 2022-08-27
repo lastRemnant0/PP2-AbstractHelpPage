84db16a6868514bc72e7adf4050bdab73d2a5357
Ali Alantaif <ali.entaif@gmail.com>
2022-08-27 13:37:03 +0300

    added menu buttons for mobile
    with queries and styles

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const menuBtn = document.getElementById("menu-btn");
-
+const barMobile = document.getElementById("topbar-mobile");
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
+  barMobile.classList.toggle("active");
 });
